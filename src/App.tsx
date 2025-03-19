c331af7afbdf269ff9ddd5172a84e903533cdaa6
Jnzinho <joaovitorjn@unochapeco.edu.br>
2025-03-19 20:44:01 -0300

    feat: dark mode and finished project

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,64 @@
 import Card from "./components/Card";
+import { useState, useEffect } from "react";
+import DarkModeToggle from "./components/DarkModeToggle";
+import logo from "./assets/logo.png";
+
+const skills = [
+  {
+    backgroundColor: "#E5532F",
+    name: "html",
+    color: "white",
+  },
+  {
+    backgroundColor: "#F7E02A",
+    name: "javascript",
+    color: "black",
+  },
+  {
+    backgroundColor: "#387CC8",
+    name: "typescript",
+    color: "white",
+  },
+  {
+    backgroundColor: "#5FD3F3",
+    name: "react",
+    color: "black",
+  },
+  {
+    backgroundColor: "#36B7F0",
+    name: "tailwind",
+    color: "white",
+  },
+  {
+    backgroundColor: "#FF485A",
+    name: "tanstack",
+    color: "white",
+  },
+];
 
 function App() {
-  const skills = [
-    {
-      backgroundColor: "red",
-      name: "hmtl",
-    },
-    {
-      backgroundColor: "yellow",
-      name: "javascript",
-    },
-    {
-      backgroundColor: "blue",
-      name: "typescript",
-    },
-  ];
+  const [isDark, setIsDark] = useState(
+    window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+
+  useEffect(() => {
+    document.body.classList.toggle("dark", isDark);
+    document.body.classList.toggle("bg-gray-800", isDark);
+    document.body.classList.toggle("text-white", isDark);
+  }, [isDark]);
 
   return (
-    <>
+    <main className={"flex items-center justify-center min-h-screen"}>
+      <img src={logo} alt="logo" className="absolute left-10 top-10 w-10" />
+      <DarkModeToggle setIsDark={setIsDark} isDark={isDark}></DarkModeToggle>
       <Card
-        text="Front end developer with passion for creating beautiful and accesible experiences"
-        title="Jubiscreudo dev"
+        text="Front end developer with passion for creating beautiful and accessible experiences"
+        title="Jnzinho"
         pills={skills}
         image="https://i.pinimg.com/736x/7a/38/84/7a38845d74e0097bcd1f2e394d1404a3.jpg"
       ></Card>
-    </>
+    </main>
   );
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,7 +18,10 @@ export default function Card({ title, text, pills, image }: CardProps) {
   const renderedPills =
     pills?.map((p) => {
       return (
-        <li className="flex flex-row">
+        <li
+          className="flex flex-row p-0 px-2 rounded-sm"
+          style={{ backgroundColor: p.backgroundColor, color: p.color }}
+        >
           <span>{p.name}</span>
           {p.emoji ? <p.emoji></p.emoji> : null}
         </li>
@@ -26,11 +29,15 @@ export default function Card({ title, text, pills, image }: CardProps) {
     }) || null;
 
   return (
-    <article className="flex flex-col gap-2 w-60 shadow-lg rounded-md">
-      <img src={image} alt="photo" className="fl"></img>
-      <h1 className="font-bold text-lg">{title}</h1>
-      <p className="text-sm">{text}</p>
-      <ul className="flex gap-3">{renderedPills}</ul>
+    <article className="flex flex-col gap-2 w-72 shadow-lg dark:shadow-gray-700 rounded-lg overflow-hidden">
+      <section>
+        <img src={image} alt="photo" className="fl"></img>
+      </section>
+      <section className="p-3 pt-1 flex flex-col gap-2">
+        <h1 className="font-bold text-lg">{title}</h1>
+        <p className="text-sm">{text}</p>
+        <ul className="flex gap-2 text-sm flex-wrap">{renderedPills}</ul>
+      </section>
     </article>
   );
 }

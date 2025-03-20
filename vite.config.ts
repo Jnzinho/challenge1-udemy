7a19dacb7b44acd437b00072cfd1eab0fc3f7325
Jnzinho <joaovitorjn@unochapeco.edu.br>
2025-03-19 21:11:31 -0300

    chore: post deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/challenge1-udemy",
+  base: "/challenge1-udemy/",
 });
